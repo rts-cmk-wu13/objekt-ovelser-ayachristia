7523cb4d74440fc4d23a782427dea3ad5e8efafc
iamayac <ayachristia@gmail.com>
2025-02-04 10:31:22 +0100

    opgave data øvelser løst

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+"use strict";
+
+const computer = {
+  brand: "Del",
+  year: 2024,
+  rentedFrom: "Roskilde Tekniske skole",
+  skærmStr: '27"',
+};
+
+console.log(computer.brand);
+console.log(computer.year);
+console.log(computer.rentedFrom);
+console.log(computer.skærmStr);
+
+console.log(computer["brand"]);
+console.log(computer["year"]);
+console.log(computer["rentedFrom"]);
+console.log(computer["skærmStr"]);
